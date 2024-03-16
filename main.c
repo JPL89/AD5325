c16a5b4278599658c5f165f918a3cb590fecb534
Josimar <josimarpereiraleite@gmail.com>
2024-03-15 21:28:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -16,33 +16,6 @@
 #include "wire.h"
 #include "ad5325.h"
 
-#define LTC2631_SLA_W 0xE6
-
-// COMMAND:
-// 0x00: Write to Input Register
-// 0x01: Update (Power Up) DAC Register
-// 0x03: Write to and Update (Power Up) DAC Register
-// 0x04: Power Down
-// 0x06: Select Internal Reference
-// 0x07: Select External Reference
-//
-void LTC2631_Write(unsigned char command, unsigned int data)
-{
-    unsigned long code = 0;
-    
-    code = command;
-    code = code << 20;
-    code |= (data << 4);
-        
-    Wire_Start();
-    Wire_Write(LTC2631_SLA_W);
-    Wire_Write((unsigned char)((code >> 24) & 0xFF));
-    Wire_Write((unsigned char)((code >> 16) & 0xFF));
-    Wire_Write((unsigned char)((code >> 8) & 0xFF));
-    Wire_Write((unsigned char)((code) & 0xFF));
-    Wire_Stop();
-}
-
 void main(void)
 {
     __delay_ms(500);   
@@ -59,17 +32,6 @@ void main(void)
                     4095);      // DATA
     
     
-    KS0066_Init();
-    KS0066_Clear(0);
-    KS0066_Clear(1);  
-    KS0066_Goto(0,0);               
-            
-    unsigned int r = AD5325_Read(0);
-    
-    KS0066_Char(48 + r / 1000 % 10);
-    KS0066_Char(48 + r / 100 % 10);
-    KS0066_Char(48 + r / 10 % 10);
-    KS0066_Char(48 + r / 1 % 10);
     
     while(1)
     {  
